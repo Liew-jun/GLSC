a45378e57990de84d8772e24a1b51de2fa4b87cf
liew <239161491@qq.com>
2020-09-12 15:43:17 +0800

    测试一下

---
+++ demo9527/src/main/java/com/example/demo9527/Demo9527Application.java
@@ -0,0 +1,13 @@
+package com.example.demo9527;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo9527Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo9527Application.class, args);
+	}
+
+}
---
+++ demo9527/src/test/java/com/example/demo9527/Demo9527ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo9527;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo9527ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
